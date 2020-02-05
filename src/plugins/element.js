44bad20f6f811db9978ea17fd89fd297a82a2efa
ma.chuang <ma.chuang@xcar.com.cn>
2020-02-05 16:18:04 +0800

    build: 增加按需引入

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
-import Element from 'element-ui'
+import { Button, Pagination, Dialog, Loading, MessageBox, Message, Menu, Submenu, MenuItem, Input, Checkbox, Select, Option } from 'element-ui'
 import '../styles/element-variables.scss'
 
-Vue.use(Element, {
-  size: 'medium' // set element-ui default size
-})
+Vue.prototype.$ELEMENT = { size: 'small', zIndex: 2000 }
+
+Vue
+  .use(Button)
+  .use(Pagination)
+  .use(Dialog)
+  .use(Menu)
+  .use(Submenu)
+  .use(MenuItem)
+  .use(Input)
+  .use(Checkbox)
+  .use(Select)
+  .use(Option)
+
+Vue.prototype.$loading = Loading.service
+Vue.prototype.$alert = MessageBox.alert
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$message = Message
 
